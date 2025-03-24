bd362500cab64dd8b1f980154a4949b1b1018853
ArnaudStigliani <arnaud.stigliani@grenoble-inp.org>
2025-03-24 13:48:29 +0100

    lots of changes

--- fig1/make_fig_1.r
+++ fig1/make_fig_1.r
@@ -6,11 +6,10 @@ library(reshape2)
 library(ggh4x)
 
 arg  <-  commandArgs(trailingOnly=TRUE) 
-lib1.dir <- "../../results/sequencing_july_2023_lib1/format_adapter_information/09-08-2023"
-lib2.dir <- "../../results/sequencing_july_2023/sequencing_july_2023_lib2/format_adapter_information/10-08-2023"
+lib1.dir <- "../mapping_preprocessing/results/format_adapter_information/lib1/" # first sequencing run = lib1, amplified libraries
+lib2.dir <- "../mapping_preprocessing/results/format_adapter_information/lib2/" # second sequencing run = lib2, non amplified libraries
 
-out_dir <-  "../../results/sequencing_july_2023" %>% file.path("shared/make_fig_1")
-## out_dir <- in_dir %>% dirname %>% dirname %>% dirname %>% file.path("shared/make_fig_1")
+out_dir <-  "./results/" %>% file.path("make_fig_1")
 dir.create(out_dir, showWarnings = FALSE, recursive = TRUE)
 
 ### read all files
@@ -29,11 +28,11 @@ trimmed <- bind_rows(lib1 = lib1.trimmed.df, lib2 = lib2.trimmed.df, .id="lib") 
 
 
 ### get total number of reads in that pass basecalling
-log.file.path.lib1 <- lib1.dir %>% dirname %>% dirname %>% file.path("pipeline_map", "09-08-2023", "trimming", "bc1_s1.log")
+log.file.path.lib1 <- lib1.dir %>% dirname %>% dirname %>% file.path("pipeline_map", "lib1"  "trimming", "bc1_s1.log")
 command.line.lib1 <- paste("head -8 ", log.file.path.lib1, " | tail -1 | awk '{print $4}' | sed 's/,//g'",  sep="")
 nread.lib1 <- system(command.line.lib1, intern = TRUE) %>% as.numeric
 
-log.file.path.lib2 <- lib2.dir %>% dirname %>% dirname %>% file.path("pipeline_map", "10-08-2023", "trimming", "bc1_s1.log")
+log.file.path.lib2 <- lib2.dir %>% dirname %>% dirname %>% file.path("pipeline_map", "lib2", "trimming", "bc1_s1.log")
 command.line.lib2 <- paste("head -8 ", log.file.path.lib2, " | tail -1 | awk '{print $4}' | sed 's/,//g'",  sep="")
 nread.lib2 <- system(command.line.lib2, intern = TRUE) %>% as.numeric
 
@@ -43,17 +42,6 @@ trimmed.nreads.df <- left_join(trimmed, nread.df) %>%
     dplyr::select(-tot_reads_trimmed) %>%
     mutate(percent_reads_trimmed = reads_trimmed/nread *100 ) %>%
     mutate(barcode = factor(barcode, levels = paste0("bc", 1:12))) 
-
-#### plot average read nb (stupid plot)
-g <- ggplot(trimmed.nreads.df %>% dplyr::filter(barcode == "bc1"), aes(x = barcode, y = nread, colour= lib)) +
-    geom_point() +
-    scale_y_continuous(breaks = nread.df$nread, labels = formatC(nread.df$nread, format='e', digits=1))+
-    ggtitle("Average read nb in\nlib1 (applified) and lib2") +
-    ylab("# of reads") +
-    theme_bw() +
-    theme(plot.title = element_text(size = 10, hjust=0.5)) +
-    theme(axis.title.x = element_blank(), axis.ticks.x = element_blank(), axis.text.x = element_blank())
-ggpubr::ggexport(g, filename = file.path(out_dir, "average_read_nb.pdf"), width = 2, height =3 )
 
 #### plot read number (not average)
 g <- ggplot(trimmed.nreads.df , aes(x = barcode, y = reads_trimmed, fill= lib)) +
@@ -66,25 +54,14 @@ g <- ggplot(trimmed.nreads.df , aes(x = barcode, y = reads_trimmed, fill= lib)) 
 ggpubr::ggexport(g, filename = file.path(out_dir, "nb_reads_2adapters.pdf"), width = 5, height =3 )
 
 
-#### plot average read nb (stupid plot)
-g <- ggplot(trimmed.nreads.df , aes(x = barcode, y = percent_reads_trimmed, fill= lib)) +
-    geom_bar(stat = "identity") +
-    ggtitle(paste0("percentage of reads with 2 adapters")) +
-    facet_grid(.~ lib, scale="free_x", space="free_x") +
-    theme_bw() +
-    theme(plot.title = element_text(size = 10, hjust=0.5)) +
-    scale_x_discrete(guide = guide_axis(n.dodge=2)) 
-ggpubr::ggexport(g, filename = file.path(out_dir, "percentage_reads_2adapters.pdf"), width = 5, height =3 )
-
-
 #### how many reads map
 mapped.lib1.name <- file.path(out_dir, "mapped_lib1.tsv")
 mapped.lib2.name <- file.path(out_dir, "mapped_lib2.tsv")
 mapped.name <- c(mapped.lib1.name, mapped.lib2.name)
 
 
-bam.lib1.name  <-  lib1.dir %>% dirname %>% dirname %>% file.path("pipeline_map", "09-08-2023", "mapping", "trimmed_primary.bam")
-bam.lib2.name  <-  lib2.dir %>% dirname %>% dirname %>% file.path("pipeline_map", "10-08-2023", "mapping", "trimmed_primary.bam")
+bam.lib1.name  <-  lib1.dir %>% dirname %>% dirname %>% file.path("pipeline_map", "lib1", "mapping", "trimmed_primary.bam")
+bam.lib2.name  <-  lib2.dir %>% dirname %>% dirname %>% file.path("pipeline_map", "lib2", "mapping", "trimmed_primary.bam")
 bam.name <- c(bam.lib1.name, bam.lib2.name)
 
 command <- paste("module load samtools;samtools view", bam.name ,
@@ -113,8 +90,8 @@ ggsave( filename = out_boxplot, g, width = 3, height = 5)
 
 #### How many reads with a polyA tail
 
-pA.lib1.name <- lib1.dir %>% dirname %>% dirname %>% file.path("characterize_polyA", "09-08-2023", "barcode_nreads_pA.tsv")
-pA.lib2.name <- lib2.dir %>% dirname %>% dirname %>% file.path("characterize_polyA", "10-08-2023", "barcode_nreads_pA.tsv")
+pA.lib1.name <- lib1.dir %>% dirname %>% dirname %>% file.path("characterize_polyA", "lib1", "barcode_nreads_pA.tsv")
+pA.lib2.name <- lib2.dir %>% dirname %>% dirname %>% file.path("characterize_polyA", "lib2", "barcode_nreads_pA.tsv")
 
 pA.name <- c(pA.lib1.name, pA.lib2.name)
 
@@ -122,8 +99,8 @@ pA.df <- lapply(pA.name, read.table, sep="\t", header=TRUE) %>%
     mapply(mutate,. ,lib=c("lib1","lib2"), SIMPLIFY=FALSE) %>% 
     do.call(rbind.data.frame, .) 
 
-pA_15.lib1.name <- lib1.dir %>% dirname %>% dirname %>% file.path("characterize_polyA", "09-08-2023", "barcode_nreads_pA_15.tsv")
-pA_15.lib2.name <- lib2.dir %>% dirname %>% dirname %>% file.path("characterize_polyA", "10-08-2023", "barcode_nreads_pA_15.tsv")
+pA_15.lib1.name <- lib1.dir %>% dirname %>% dirname %>% file.path("characterize_polyA", "lib1", "barcode_nreads_pA_15.tsv")
+pA_15.lib2.name <- lib2.dir %>% dirname %>% dirname %>% file.path("characterize_polyA", "lib2", "barcode_nreads_pA_15.tsv")
 
 pA_15.name <- c(pA_15.lib1.name, pA_15.lib2.name)
 
--- mapping_preprocessing/run_all.sh
+++ mapping_preprocessing/run_all.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+
+### sequencing run is either lib1 (amplified) or lib1 (non amplified) or R10 (amplified and sequenced on R10 flowcell)
+
 sequencing_run=$1
 fast5_dir=$2
 
 
-pipeline_map.sh  $sequencing_run
-format_adapter_informations.sh $sequencing_run
-analyze_adapter_informations.r $sequencing_run
-demultiplex_read_names.sh $sequencing_run
-demultiplex_reads_from_fast5.sh $sequencing_run $fast5_dir
-get_polyA_tailfindR.r $sequencing_run
-characterize_polyA.r $sequencing_run
-get_polyA_bam_files.sh $sequencing_run
+./pipeline_map.sh  $sequencing_run
+./format_adapter_informations.sh $sequencing_run
+./analyze_adapter_informations.r $sequencing_run
+./demultiplex_read_names.sh $sequencing_run
+./demultiplex_reads_from_fast5.sh $sequencing_run $fast5_dir
+./get_polyA_tailfindR.r $sequencing_run ## Don't run if R10
+
+./get_pA_reads_R10.sh ## run if R10
 
 
 exit 0
